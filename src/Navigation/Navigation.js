d4ed0dbac3083aed95f2496955f30667fad067bb
ahmadawss <ahmad-awss@hotmail.com>
2020-05-21 23:40:25 +0300

    Added github buttons

--- src/Navigation/Navigation.js
+++ src/Navigation/Navigation.js
@@ -2,12 +2,26 @@ import React from 'react';
 import { useTranslation } from "react-i18next";
 import './Navigation.css';
 
-export default function Navigation(){
+export default function Navigation() {
     const { t } = useTranslation();
 
     return (
         <div className="navigation">
             <div className="site-name">{t("SiteName")}</div>
+            <div className="github">
+                <a
+                    href="https://github.com/ahmadmuhsen/ogame-acs-split"
+                    className="repo"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                ><i className="fab fa-github" />{t("Repository")}</a>
+                <a
+                    href="https://github.com/ahmadmuhsen/ogame-acs-split/issues"
+                    className="issue"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                ><i className="fas fa-bug" />{t("ReportIssue")}</a>
+            </div>
         </div>
     )
 }
