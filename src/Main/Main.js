8020ac258499fd6842c651d60c05eb467e97876d
ahmadmuhsen <ahmad-muhsen@outlook.com>
2020-05-30 18:31:03 +0300

    Scrapped whole design

--- src/Main/Main.js
+++ src/Main/Main.js
@@ -1,11 +1,7 @@
 import React, { useState, useEffect } from 'react';
 
-import SectionTitle from '../Components/SectionTitle/SectionTitle';
 import TextInput from '../Components/TextInput/TextInput';
 import Button from '../Components/Button/Button';
-import PlayerPanel from '../Components/PlayerPanel/PlayerPanel';
-import ACSAttackResult from '../Components/ACSAttackResults/ACSAttackResults';
-import PanelStatisticsLabel from '../Components/PanelStatisticsLabel/PanelStatisticsLabel';
 
 import { useTranslation } from "react-i18next";
 import { GetCombatReport } from './GetCombatReport';
@@ -20,12 +16,10 @@ export default function Main() {
     const [CombatReports, setCombatReports] = useState([]);
     const [Attackers, setAttackers] = useState([]);
     const [Defenders, setDefenders] = useState([]);
-    const [AttackersPanels, setAttackersPanels] = useState([]);
-    const [DefendersPanels, setDefendersPanels] = useState([])
-    const [ApiKeyInput, setApiKeyInput] = useState("")
-    const [ApiKeyInputValidity, setApiKeyInputValidity] = useState(true)
-    const [ApiKeyInputValidityMessage, setApiKeyInputValidityMessage] = useState("")
-    const [Loading, setLoading] = useState(false)
+    const [ApiKeyInput, setApiKeyInput] = useState("");
+    const [ApiKeyInputValidity, setApiKeyInputValidity] = useState(true);
+    const [ApiKeyInputValidityMessage, setApiKeyInputValidityMessage] = useState("");
+    const [Loading, setLoading] = useState(false);
 
     const GetReport = () => {
         let keyArray = ApiKeyInput.split('-');
@@ -61,31 +55,12 @@ export default function Main() {
         let defenderPanels = [];
 
         Attackers.forEach((attacker, id) => {
-            attackerPanels.push((
-                <PlayerPanel
-                    combatReports={CombatReports}
-                    player={attacker}
-                    id={id}
-                    key={id}
-                    isDefender={false}
-                    isCoordinateOwner={false}
-                />));
         })
 
         Object.keys(Defenders).forEach((id, index) => {
-            defenderPanels.push((
-                <PlayerPanel
-                    combatReports={CombatReports}
-                    player={Defenders[id]}
-                    id={id}
-                    key={id}
-                    isDefender={true}
-                    isCoordinateOwner={index === 0 ? true : false}
-                />));
+            
         })
 
-        setAttackersPanels(attackerPanels);
-        setDefendersPanels(defenderPanels);
     }, [Attackers, Defenders]);
 
     return (
@@ -116,17 +91,6 @@ export default function Main() {
             <div className="api-key-validity-message">
                 {t(ApiKeyInputValidityMessage)}
             </div>
-            <SectionTitle title="ACS Attack Results" icon="users" />
-            <ACSAttackResult combatReports={CombatReports} />
-
-            <PanelStatisticsLabel hidden={CombatReports.length > 0 ? false : true}/>
-
-            <SectionTitle title="Attackers" icon="fighter-jet" />
-            {AttackersPanels}
-
-            <SectionTitle title="Defenders" icon="shield-alt" />
-            {DefendersPanels}
-
         </div>
     )
 }
