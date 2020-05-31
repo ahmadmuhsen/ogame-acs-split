e75c5b7fe942f24a232af1f166e8b21b1c6a701f
ahmadmuhsen <ahmad-muhsen@outlook.com>
2020-05-31 20:27:18 +0300

    ShipType and OwnerId are now part of the object itself instead of being the index

--- src/Main/GetCombatReport.js
+++ src/Main/GetCombatReport.js
@@ -53,55 +53,66 @@ export const GetCombatReport = (combatReport, CombatReports, setCombatReports, s
         .catch(function (error) {
             setLoading(false);
             setApiKeyInputValidity(false);
-            console.log(error.response);
-            switch (error.response.status) {
-                case 400:
-                    setApiKeyInputValidityMessage("InvalidKey");
-                    break;
-                case 500:
-                    setApiKeyInputValidityMessage("ServerError");
-                    break;
-                default:
-                    setApiKeyInputValidityMessage("UnknownError");
-            }
+            console.log(error);
+            if (error.response)
+                switch (error.response.status) {
+                    case 400:
+                        setApiKeyInputValidityMessage("InvalidKey");
+                        break;
+                    case 500:
+                        setApiKeyInputValidityMessage("ServerError");
+                        break;
+                    default:
+                        setApiKeyInputValidityMessage("UnknownError");
+                }
+            else
+                setApiKeyInputValidityMessage("UnknownError");
         })
 }
 
 function ParseFleeterData(data, finalRound, isDefender, repairedDefences) {
     let fleeters = [];
     data.forEach(item => {
-        if (!fleeters[item.fleet_owner_id]) {
-            fleeters[item.fleet_owner_id] = {
+
+        if (!fleeters.some(fltr => fltr.ownerId === item.fleet_owner_id)) {
+            fleeters.push({
+                ownerId: item.fleet_owner_id,
                 name: item.fleet_owner,
                 alliance: item.fleet_owner_alliance_tag,
                 fleet: []
-            }
+            })
         }
-        item.fleet_composition.forEach(fleet => {
-            if (!fleeters[item.fleet_owner_id].fleet[fleet.ship_type])
-                fleeters[item.fleet_owner_id].fleet[fleet.ship_type] = {
-                    preCount: fleet.count
-                }
-            else {
-                fleeters[item.fleet_owner_id].fleet[fleet.ship_type].preCount += fleet.count;
+
+        item.fleet_composition.forEach(fltcmp => {
+            let fleeterIndex = fleeters.findIndex(fltr => fltr.ownerId === item.fleet_owner_id);
+
+            if (!fleeters[fleeterIndex].fleet.some(flt => flt.shipType === fltcmp.ship_type)) {
+                fleeters[fleeterIndex].fleet.push({
+                    shipType: fltcmp.ship_type,
+                    preCount: fltcmp.count,
+                    postCount: fltcmp.count
+                })
             }
-            fleeters[item.fleet_owner_id].fleet[fleet.ship_type].postCount = fleeters[item.fleet_owner_id].fleet[fleet.ship_type].preCount;
+            else {
+                let fleetIndex = fleeters[fleeterIndex].fleet.findIndex(flt => flt.shipType === fltcmp.ship_type);
+                fleeters[fleeterIndex].fleet[fleetIndex].preCount += fltcmp.count;
+                fleeters[fleeterIndex].fleet[fleetIndex].postCount = fleeters[fleeterIndex].fleet[fleetIndex].preCount;
+            }
         })
     });
 
     if (finalRound.length > 0) {
         finalRound.forEach(ship => {
-            let itemID = data[ship.owner].fleet_owner_id;
-            fleeters[itemID].fleet[ship.ship_type].postCount = ship.count;
+            let fleeterIndex = fleeters.findIndex(fltr => fltr.ownerId === data[ship.owner].fleet_owner_id);
+            let fleetIndex = fleeters[fleeterIndex].fleet.findIndex(flt => flt.shipType === ship.ship_type);
+            fleeters[fleeterIndex].fleet[fleetIndex].postCount = ship.count;
         })
     }
 
     if (isDefender)
         repairedDefences.forEach(repaired => {
-            let fleeterids = Object.keys(fleeters);
-            if (fleeterids.length > 0 && fleeters[fleeterids[0]].fleet[repaired.repaired_type]) {
-                fleeters[fleeterids[0]].fleet[repaired.repaired_type].postCount = repaired.repaired_count;
-            }
+            let fleetIndex = fleeters[0].fleet.findIndex(flt => flt.shipType === repaired.repaired_type);
+            fleeters[0].fleet[fleetIndex].postCount = repaired.repaired_count;
         })
     return fleeters;
 }
