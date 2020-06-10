ca764923440db63a3514b16f87321589e05c4ae7
ahmadmuhsen <ahmad-muhsen@outlook.com>
2020-06-10 09:35:32 +0300

    Because it was bugging me

--- src/Components/AttackFleetDetails/AttackFleetDetails.js
+++ src/Components/AttackFleetDetails/AttackFleetDetails.js
@@ -52,88 +52,89 @@ export default function AttackFleetDetails({
 
     return (
         <div className="attack-fleet-details">
-
-            <div className="player-name">{attackFleet.alliance ? `[${attackFleet.alliance}]` : "attackFleet.alliance"} {attackFleet.name}</div>
-            <div className="hyperspace-tech">
-                {t("HyperspaceTech")}
-                <TextInput
-                    value={attackFleet.hyperspaceTech}
-                    onChange={event => onHyperTechnologyChange(crIndex, index, parseInt(event.target.value))}
-                    valid={attackFleet.hyperspaceTech <= 0 ? false : true}
-                />
-            </div>
-
-            <div className="label">{t("Loot")}</div>
-            <div className="one-input-info">
-                {t("Metal")}
-                {
-                    isACS ?
-                        <div>{FormatUnits(Math.round(attackFleet.metalLoot))}</div> :
-                        <TextInput
-                            value={Math.round(attackFleet.metalLoot)}
-                            onChange={event => onLootChange(crIndex, index, "metalLoot", parseInt(event.target.value))}
-                            valid={attackFleet.metalLoot < 0 ? false : true}
-                        />
-
-                }
-            </div>
-            <div className="one-input-info">
-                {t("Crystal")}
-                {
-                    isACS ?
-                        <div>{FormatUnits(Math.round(attackFleet.crystalLoot))}</div> :
-                        <TextInput
-                            value={Math.round(attackFleet.crystalLoot)}
-                            onChange={event => onLootChange(crIndex, index, "crystalLoot", parseInt(event.target.value))}
-                            valid={attackFleet.crystalLoot < 0 ? false : true}
-                        />
-                }
-            </div>
-            <div className="one-input-info">
-                {t("Deuterium")}
-                {
-                    isACS ?
-                        <div>{FormatUnits(Math.round(attackFleet.deuteriumLoot))}</div> :
-                        <TextInput
-                            value={Math.round(attackFleet.deuteriumLoot)}
-                            onChange={event => onLootChange(crIndex, index, "deuteriumLoot", parseInt(event.target.value))}
-                            valid={attackFleet.deuteriumLoot < 0 ? false : true}
-                        />
-                }
-            </div>
-
-            <div className="label">{t("FlightConsumption")}</div>
-            <div className="one-input-info">
-                {t("Deuterium")}
-                <TextInput
-                    value={Math.round(attackFleet.deuteriumConsumption)}
-                    onChange={event => onDeuteriumConsumptionChange(crIndex, index, parseInt(event.target.value))}
-                    valid={attackFleet.deuteriumConsumption <= 0 ? false : true}
-                />
-            </div>
-
-            <div className="label">{t("FleetComposition")}</div>
-            <div className="fleet-composition">
-                <div className="ship-name">{t("Ship")}</div>
-                <div className="ship-count">
-                    <div>{t("PreAttack")}</div>
-                    <div>{t("PostAttack")}</div>
+            <div className="card">
+                <div className="player-name">{attackFleet.alliance ? `[${attackFleet.alliance}]` : "attackFleet.alliance"} {attackFleet.name}</div>
+                <div className="hyperspace-tech">
+                    {t("HyperspaceTech")}
+                    <TextInput
+                        value={attackFleet.hyperspaceTech}
+                        onChange={event => onHyperTechnologyChange(crIndex, index, parseInt(event.target.value))}
+                        valid={attackFleet.hyperspaceTech <= 0 ? false : true}
+                    />
                 </div>
-            </div>
-            {FleetComposition}
 
-            <div className="label">{t("UnitsLost")}</div>
-            <div className="one-input-info">
-                <div>{t("Metal")}</div>
-                <div>{FormatUnits(attackFleet.unitsLost.metal)}</div>
-            </div>
-            <div className="one-input-info">
-                <div>{t("Crystal")}</div>
-                <div>{FormatUnits(attackFleet.unitsLost.crystal)}</div>
-            </div>
-            <div className="one-input-info">
-                <div>{t("Deuterium")}</div>
-                <div>{FormatUnits(attackFleet.unitsLost.deuterium)}</div>
+                <div className="label">{t("Loot")}</div>
+                <div className="one-input-info">
+                    {t("Metal")}
+                    {
+                        isACS ?
+                            <div>{FormatUnits(Math.round(attackFleet.metalLoot))}</div> :
+                            <TextInput
+                                value={Math.round(attackFleet.metalLoot)}
+                                onChange={event => onLootChange(crIndex, index, "metalLoot", parseInt(event.target.value))}
+                                valid={attackFleet.metalLoot < 0 ? false : true}
+                            />
+
+                    }
+                </div>
+                <div className="one-input-info">
+                    {t("Crystal")}
+                    {
+                        isACS ?
+                            <div>{FormatUnits(Math.round(attackFleet.crystalLoot))}</div> :
+                            <TextInput
+                                value={Math.round(attackFleet.crystalLoot)}
+                                onChange={event => onLootChange(crIndex, index, "crystalLoot", parseInt(event.target.value))}
+                                valid={attackFleet.crystalLoot < 0 ? false : true}
+                            />
+                    }
+                </div>
+                <div className="one-input-info">
+                    {t("Deuterium")}
+                    {
+                        isACS ?
+                            <div>{FormatUnits(Math.round(attackFleet.deuteriumLoot))}</div> :
+                            <TextInput
+                                value={Math.round(attackFleet.deuteriumLoot)}
+                                onChange={event => onLootChange(crIndex, index, "deuteriumLoot", parseInt(event.target.value))}
+                                valid={attackFleet.deuteriumLoot < 0 ? false : true}
+                            />
+                    }
+                </div>
+
+                <div className="label">{t("FlightConsumption")}</div>
+                <div className="one-input-info">
+                    {t("Deuterium")}
+                    <TextInput
+                        value={Math.round(attackFleet.deuteriumConsumption)}
+                        onChange={event => onDeuteriumConsumptionChange(crIndex, index, parseInt(event.target.value))}
+                        valid={attackFleet.deuteriumConsumption <= 0 ? false : true}
+                    />
+                </div>
+
+                <div className="label">{t("FleetComposition")}</div>
+                <div className="fleet-composition">
+                    <div className="ship-name">{t("Ship")}</div>
+                    <div className="ship-count">
+                        <div>{t("PreAttack")}</div>
+                        <div>{t("PostAttack")}</div>
+                    </div>
+                </div>
+                {FleetComposition}
+
+                <div className="label">{t("UnitsLost")}</div>
+                <div className="one-input-info">
+                    <div>{t("Metal")}</div>
+                    <div>{FormatUnits(attackFleet.unitsLost.metal)}</div>
+                </div>
+                <div className="one-input-info">
+                    <div>{t("Crystal")}</div>
+                    <div>{FormatUnits(attackFleet.unitsLost.crystal)}</div>
+                </div>
+                <div className="one-input-info">
+                    <div>{t("Deuterium")}</div>
+                    <div>{FormatUnits(attackFleet.unitsLost.deuterium)}</div>
+                </div>
             </div>
         </div>
     )
