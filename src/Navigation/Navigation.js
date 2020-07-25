bb1cc46695a542a954e2168d7ba0506958c04525
ahmadmuhsen <ahmad-muhsen@outlook.com>
2020-07-25 11:30:18 +0300

    Language Support

--- src/Navigation/Navigation.js
+++ src/Navigation/Navigation.js
@@ -1,14 +1,58 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useTranslation } from "react-i18next";
+import i18next from "i18next";
 import './Navigation.css';
 
 export default function Navigation() {
+    const [Language, setLanguage] = useState(localStorage.preferredLanguage);
+    const [LanguageSelect, setLanguageSelect] = useState([])
+    const [SelectVisible, setSelectVisible] = useState(false)
     const { t } = useTranslation();
+    const AllLanguages = ["en"]
+
+    useEffect(() => {
+        localStorage.preferredLanguage = Language;
+        i18next.changeLanguage(Language)
+
+        let otherLanguages = AllLanguages.filter(lng => lng !== Language);
+        setLanguageSelect(otherLanguages.map(lng => (
+            <div
+                className="language-option"
+                onClick={() => setLanguage(lng)}
+                key={`LANGUAGE${lng}`}
+            >
+                {t(lng)}
+            </div>
+        )))
+        setSelectVisible(false)
+    }, [Language])
 
     return (
         <div className="navigation">
             <div className="site-name">{t("SiteName")}</div>
-            <div className="github">
+            <div className="right-toolbar">
+                <div className="language">
+                    <div
+                        className="selected-language"
+                        onClick={() => setSelectVisible(!SelectVisible)}
+                    >
+                        <i className="fas fa-globe-asia" />
+                        {t(Language)}
+                    </div>
+                    {SelectVisible ?
+                        <div className="language-options">
+                            {LanguageSelect}
+                            <a
+                                className="language-option"
+                                href="https://github.com/ahmadmuhsen/ogame-acs-split/wiki/Adding-Languages"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            >
+                                <i className="fas fa-file-alt" />
+                                {t("AddEditLanguage")}
+                            </a>
+                        </div> : ""}
+                </div>
                 <a
                     href="https://github.com/ahmadmuhsen/ogame-acs-split"
                     className="repo"
