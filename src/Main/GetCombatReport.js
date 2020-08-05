671c8d0da7341c2bf8f2becf31b68a87df26944c
ahmadmuhsen <ahmad-muhsen@outlook.com>
2020-08-05 13:35:34 +0300

    Keys are allowed to be added only once

--- src/Main/GetCombatReport.js
+++ src/Main/GetCombatReport.js
@@ -12,74 +12,80 @@ export const GetCombatReport = (
     settingsData,
     setSettingsData
 ) => {
-    setLoading(true);
-    const request = {
-        method: "GET",
-        url: `${config.api}/api/combat?combatReport=${combatReport}`
-    };
+    let exists = CombatReports.find(cr => cr.key === combatReport);
+    if (!exists) {
+        setLoading(true);
+        const request = {
+            method: "GET",
+            url: `${config.api}/api/combat?combatReport=${combatReport}`
+        };
 
-    axios(request)
-        .then(function (response) {
-            let data = response.data;
+        axios(request)
+            .then(function (response) {
+                let data = response.data;
 
-            if (CombatReports.length > 0 && CombatReports[0].coordinates !== data.generic.combat_coordinates) {
-                setApiKeyInputValidity(false);
-                setApiKeyInputValidityMessage("InvalidCoordinates");
-                setLoading(false);
-            } else {
-                let crdata = {};
-                crdata.key = data.Id;
-                crdata.coordinates = data.generic.combat_coordinates;
+                if (CombatReports.length > 0 && CombatReports[0].coordinates !== data.generic.combat_coordinates) {
+                    setApiKeyInputValidity(false);
+                    setApiKeyInputValidityMessage("InvalidCoordinates");
+                    setLoading(false);
+                } else {
+                    let crdata = {};
+                    crdata.key = data.Id;
+                    crdata.coordinates = data.generic.combat_coordinates;
 
-                crdata.metalLoot = data.generic.loot_metal;
-                crdata.crystalLoot = data.generic.loot_crystal;
-                crdata.deuteriumLoot = data.generic.loot_deuterium;
-                crdata.debrisMetal = data.generic.debris_metal;
-                crdata.debrisCrystal = data.generic.debris_crystal;
-                crdata.debrisReaperMetal = data.generic.debris_reaper_metal_retrieved;
-                crdata.debrisReaperCrystal = data.generic.debris_reaper_crystal_retrieved;
+                    crdata.metalLoot = data.generic.loot_metal;
+                    crdata.crystalLoot = data.generic.loot_crystal;
+                    crdata.deuteriumLoot = data.generic.loot_deuterium;
+                    crdata.debrisMetal = data.generic.debris_metal;
+                    crdata.debrisCrystal = data.generic.debris_crystal;
+                    crdata.debrisReaperMetal = data.generic.debris_reaper_metal_retrieved;
+                    crdata.debrisReaperCrystal = data.generic.debris_reaper_crystal_retrieved;
 
-                crdata.attackers = ParseFleeterData(
-                    data.attackers,
-                    data.rounds.length > 0 ? data.rounds[data.rounds.length - 1].attacker_ships : [],
-                    false,
-                    data.repaired_defenses
-                );
+                    crdata.attackers = ParseFleeterData(
+                        data.attackers,
+                        data.rounds.length > 0 ? data.rounds[data.rounds.length - 1].attacker_ships : [],
+                        false,
+                        data.repaired_defenses
+                    );
 
-                crdata.defenders = ParseFleeterData(
-                    data.defenders,
-                    data.rounds.length > 0 ? data.rounds[data.rounds.length - 1].defender_ships : [],
-                    true,
-                    data.repaired_defenses
-                );
+                    crdata.defenders = ParseFleeterData(
+                        data.defenders,
+                        data.rounds.length > 0 ? data.rounds[data.rounds.length - 1].defender_ships : [],
+                        true,
+                        data.repaired_defenses
+                    );
 
-                let crs = [...CombatReports];
-                crs.push(crdata);
-                setApiKeyInputValidity(true);
-                setApiKeyInputValidityMessage("");
-                SetResourceStatistics(crs, settingsData);
-                setCombatReports(crs);
-                setSettingsData({...settingsData, probeStorage: crdata.probe_storage})
-            }
-        })
-        .catch(function (error) {
-            setLoading(false);
-            setApiKeyInputValidity(false);
-            console.log(error);
-            if (error.response)
-                switch (error.response.status) {
-                    case 400:
-                        setApiKeyInputValidityMessage("InvalidKey");
-                        break;
-                    case 500:
-                        setApiKeyInputValidityMessage("ServerError");
-                        break;
-                    default:
-                        setApiKeyInputValidityMessage("UnknownError");
+                    let crs = [...CombatReports];
+                    crs.push(crdata);
+                    setApiKeyInputValidity(true);
+                    setApiKeyInputValidityMessage("");
+                    SetResourceStatistics(crs, settingsData);
+                    setCombatReports(crs);
+                    setSettingsData({ ...settingsData, probeStorage: crdata.probe_storage })
                 }
-            else
-                setApiKeyInputValidityMessage("UnknownError");
-        })
+            })
+            .catch(function (error) {
+                setLoading(false);
+                setApiKeyInputValidity(false);
+                console.log(error);
+                if (error.response)
+                    switch (error.response.status) {
+                        case 400:
+                            setApiKeyInputValidityMessage("InvalidKey");
+                            break;
+                        case 500:
+                            setApiKeyInputValidityMessage("ServerError");
+                            break;
+                        default:
+                            setApiKeyInputValidityMessage("UnknownError");
+                    }
+                else
+                    setApiKeyInputValidityMessage("UnknownError");
+            })
+    } else {
+        setApiKeyInputValidity(false);
+        setApiKeyInputValidityMessage("KeyAlreadyExists");
+    }
 }
 
 function ParseFleeterData(data, finalRound, isDefender, repairedDefences) {
