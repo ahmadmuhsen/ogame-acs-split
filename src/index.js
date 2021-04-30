5b772064baf196082ab4888e3b86c0b539e7ecd2
Ahmad Muhsen <ahmad-muhsen@outlook.com>
2021-04-30 21:44:19 +0300

    French language support

--- src/index.js
+++ src/index.js
@@ -7,12 +7,14 @@ import { initReactI18next } from "react-i18next";
 import English from "./I18n/English.json";
 import Balkan from "./I18n/Balkan.json";
 import Danish from "./I18n/Danish.json";
+import French from "./I18n/French.json";
 
 i18n.use(initReactI18next).init({
   resources: {
     en: English,
     ba: Balkan,
     da: Danish,
+    fr: French
   },
   lng: getPreferredLanguage(),
   fallbackLng: "en",
